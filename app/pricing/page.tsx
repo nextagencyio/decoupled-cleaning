1044c7842911db8bb289f83001328de52aafdc82
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:45 -0500

    fix: mark header-dependent routes as dynamic

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import PricingPlanCard from '../components/PricingPlanCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Pricing Plans | Sparkle & Shine Cleaning',
